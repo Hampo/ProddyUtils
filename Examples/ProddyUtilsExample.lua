ab33e599cde2874750c2aa731ddf659df3e2a1f5
Proddy3 <tehmcterrorist@gmail.com>
2020-04-27 01:59:57 +0100

    Initial Release
    Initial Release

---
+++ Examples/ProddyUtilsExample.lua
@@ -0,0 +1,58 @@
+if not package.cpath:find(os.getenv("APPDATA") .. "\\PopstarDevs\\2Take1Menu\\scripts\\lib\\?.dll", 1, true) then package.cpath = package.cpath .. ";" .. os.getenv("APPDATA") .. "\\PopstarDevs\\2Take1Menu\\scripts\\lib\\?.dll" end
+local ProddyUtils = require("ProddyUtils")
+
+-- ProddyUtils.Clipboard
+local ClipboardText = ProddyUtils.Clipboard.GetText()
+if ClipboardText ~= nil then
+	-- Do something with the text.
+else
+	-- Clipboard was either empty, or not text (IE: image).
+end
+
+local SomeText = "Something to save"
+if ProddyUtils.Clipboard.SetText(SomeText) then
+	-- Set clipboard text successfully.
+else
+	-- There was some error setting the text.
+end
+
+-- ProddyUtils.MessageBox
+if ProddyUtils.MessageBox.Show("Do you like pie?", "Important question", ProddyUtils.MessageBox.Buttons.YesNo) == ProddyUtils.MessageBox.DialogResult.Yes then
+	-- User likes pie.
+else
+	-- User doesn't like pie.
+end
+--[[ You do not need to include this in your script, but these are the contents of the two tables (enums):
+	ProddyUtils.MessageBox.Buttons = {
+		["OK"] = 1,
+		["OKCancel"] = 2,
+		["RetryCancel"] = 3,
+		["YesNo"] = 4,
+		["YesNoCancel"] = 5
+	}
+	ProddyUtils.MessageBox.DialogResult = {
+		["OK"] = 1,
+		["Cancel"] = 2,
+		["Retry"] = 3,
+		["Yes"] = 4,
+		["No"] = 5
+	}
+]]
+
+-- ProddyUtils.IO
+if ProddyUtils.IO.CreateDirectory(os.getenv("APPDATA") .. "\\PopstarDevs\\2Take1Menu\\scripts\\lib\\ProddyUtils") then
+	-- Directory already exists or was created successfully.
+else
+	-- Directory couldn't be created. Probably invalid path or permissions.
+end
+
+local Exists, IsDir = ProddyUtils.IO.Exists(os.getenv("APPDATA") .. "\\PopstarDevs\\2Take1Menu\\scripts\\lib\\ProddyUtils")
+if Exists then
+	if IsDir then
+		-- Exists and is directory.
+	else
+		-- Exists and is file.
+	end
+else
+	-- Directory or file doesn't exist on user's system.
+end
---
+++ ProddyUtils/ProddyUtils/ProddyUtils.cpp
@@ -0,0 +1,368 @@
+#include <windows.h>
+#include <string>
+#include "lua.hpp"
+
+#pragma region UTFUtils
+#ifdef _WIN64
+#pragma warning(push)
+#pragma warning(disable : 4267)
+#endif
+std::string UTF16ToUTF8(const wchar_t* pszString, size_t Length)
+{
+	if (Length == 0)
+		return "";
+	int iLength = WideCharToMultiByte(CP_UTF8, 0, pszString, Length, nullptr, 0, nullptr, nullptr);
+	if (iLength == 0)
+		abort();
+	char* pszString2 = new char[iLength];
+	int iResult = WideCharToMultiByte(CP_UTF8, 0, pszString, Length, pszString2, iLength, nullptr, nullptr);
+	if (iResult == 0)
+	{
+		delete[] pszString2;
+		abort();
+	}
+	std::string strString(pszString2, iLength);
+	delete[] pszString2;
+	return strString;
+}
+#ifdef _WIN64
+#pragma warning(pop)
+#endif
+
+inline std::string UTF16ToUTF8(const std::wstring& strString)
+{
+	return UTF16ToUTF8(strString.c_str(), strString.size());
+}
+
+#ifdef _WIN64
+#pragma warning(push)
+#pragma warning(disable : 4267)
+#endif
+std::wstring UTF8ToUTF16(const char* pszString, size_t Length)
+{
+	if (Length == 0)
+		return L"";
+	int iLength = MultiByteToWideChar(CP_UTF8, 0, pszString, Length, nullptr, 0);
+	if (iLength == 0)
+		abort();
+	wchar_t* pszString2 = new wchar_t[iLength];
+	int iResult = MultiByteToWideChar(CP_UTF8, 0, pszString, Length, pszString2, iLength);
+	if (iResult == 0)
+	{
+		delete[] pszString2;
+		abort();
+	}
+	std::wstring strString(pszString2, iLength);
+	delete[] pszString2;
+	return strString;
+}
+#ifdef _WIN64
+#pragma warning(pop)
+#endif
+inline void lua_pushlstring(lua_State* L, const std::string& str)
+{
+	lua_pushlstring(L, str.c_str(), str.size());
+}
+
+inline void lua_pushlstring(lua_State* L, const std::wstring& str)
+{
+	lua_pushlstring(L, UTF16ToUTF8(str));
+}
+#pragma endregion
+
+#pragma region Clipboard
+bool SetClipboard(const std::wstring& str)
+{
+	if (!OpenClipboard(nullptr))
+		return false;
+	size_t Length = str.size() + 1;
+	HGLOBAL hMem = GlobalAlloc(GMEM_MOVEABLE, Length * sizeof(wchar_t));
+	if (hMem == nullptr)
+	{
+		CloseClipboard();
+		return false;
+	}
+	void* pMem = GlobalLock(hMem);
+	if (pMem == nullptr)
+	{
+		GlobalFree(hMem);
+		CloseClipboard();
+		return false;
+	}
+	memcpy(pMem, str.c_str(), Length * sizeof(wchar_t));
+	if (!GlobalUnlock(hMem))
+	{
+		HRESULT hr = HRESULT_FROM_WIN32(GetLastError());
+		if (FAILED(hr))
+		{
+			GlobalFree(hMem);
+			CloseClipboard();
+			return false;
+		}
+	}
+	if (!EmptyClipboard())
+	{
+		GlobalFree(hMem);
+		CloseClipboard();
+		return false;
+	}
+	if (SetClipboardData(CF_UNICODETEXT, hMem) == nullptr)
+	{
+		GlobalFree(hMem);
+		CloseClipboard();
+		return false;
+	}
+	CloseClipboard();
+	return true;
+}
+
+bool ClipboardContainsText(void)
+{
+	return !!IsClipboardFormatAvailable(CF_UNICODETEXT);
+}
+
+bool GetClipboardText(std::wstring* pstr)
+{
+	if (!OpenClipboard(nullptr))
+		return false;
+	if (!ClipboardContainsText())
+	{
+		CloseClipboard();
+		return false;
+	}
+	HGLOBAL hMem = GetClipboardData(CF_UNICODETEXT);
+	if (hMem == nullptr)
+	{
+		CloseClipboard();
+		return false;
+	}
+	void* pMem = GlobalLock(hMem);
+	if (pMem == nullptr)
+	{
+		CloseClipboard();
+		return false;
+	}
+	*pstr = (wchar_t*)pMem;
+	GlobalUnlock(hMem);
+	CloseClipboard();
+	return true;
+}
+
+static int lua_setclipboard(lua_State* L)
+{
+	size_t len;
+	auto text = luaL_checklstring(L, 1, &len);
+	lua_pushboolean(L, SetClipboard(UTF8ToUTF16(text, len)));
+	return 1;
+}
+
+static int lua_getclipboard(lua_State* L)
+{
+	std::wstring str;
+	if (GetClipboardText(&str)) {
+		lua_pushlstring(L, str);
+	}
+	else {
+		lua_pushnil(L);
+	}
+	return 1;
+}
+#pragma endregion
+
+#pragma region MessageBox
+static int lua_msgbox(lua_State* L)
+{
+	size_t msgLen;
+	size_t capLen;
+	auto message = luaL_checklstring(L, 1, &msgLen);
+	auto caption = luaL_optlstring(L, 2, "2Take1Menu - ProddyUtils", &capLen);
+	auto uType = MB_TASKMODAL;
+	auto type = luaL_optinteger(L, 3, 1);
+	switch (type) {
+	default:
+		uType = uType | MB_OK;
+		break;
+	case 2:
+		uType = uType | MB_OKCANCEL;
+		break;
+	case 3:
+		uType = uType | MB_RETRYCANCEL;
+		break;
+	case 4:
+		uType = uType | MB_YESNO;
+		break;
+	case 5:
+		uType = uType | MB_YESNOCANCEL;
+		break;
+	}
+	int result = MessageBox(NULL, UTF8ToUTF16(message, msgLen).c_str(), UTF8ToUTF16(caption, capLen).c_str(), uType);
+	switch (result) {
+	default:
+		lua_pushinteger(L, 1);
+		break;
+	case 2:
+		lua_pushinteger(L, 2);
+		break;
+	case 4:
+		lua_pushinteger(L, 3);
+		break;
+	case 6:
+		lua_pushinteger(L, 4);
+		break;
+	case 7:
+		lua_pushinteger(L, 5);
+		break;
+	}
+	return 1;
+}
+#pragma endregion
+
+#pragma region IO
+bool exists(const std::wstring& strPath, bool& isDir)
+{
+	DWORD dw = GetFileAttributesW(strPath.c_str());
+	if (dw != INVALID_FILE_ATTRIBUTES)
+	{
+		auto bDirectory = (dw & FILE_ATTRIBUTE_DIRECTORY) != 0;
+		isDir = bDirectory;
+		return true;
+	}
+	else
+		return false;
+}
+
+static int lua_createdirectory(lua_State* L)
+{
+	size_t len;
+	auto text = luaL_checklstring(L, 1, &len);
+	if (len == 0)
+	{
+		lua_pushboolean(L, false);
+		return 1;
+	}
+	auto strPath = UTF8ToUTF16(text, len);
+	auto isDir = false;
+	if (exists(strPath, isDir) && isDir)
+	{
+		lua_pushboolean(L, true);
+		return 1;
+	}
+	size_t pos = (len >= 3 && strPath.substr(1, 1) == L":")?strPath.find_first_of(L"\\/", 1):0;
+	do {
+		pos = strPath.find_first_of(L"\\/", pos + 1);
+		if (!CreateDirectoryW(strPath.substr(0, pos).c_str(), nullptr)) {
+			DWORD err = GetLastError();
+			if (err != ERROR_ALREADY_EXISTS) {
+				lua_pushboolean(L, false);
+				return 1;
+			}
+		}
+	} while (pos != std::string::npos);
+	lua_pushboolean(L, true);
+	return 1;
+}
+
+static int lua_exists(lua_State* L)
+{
+	size_t len;
+	auto text = luaL_checklstring(L, 1, &len);
+	auto strPath = UTF8ToUTF16(text, len);
+	auto bDirectory = false;
+	auto bExists = exists(strPath, bDirectory);
+	lua_pushboolean(L, true);
+	lua_pushboolean(L, bDirectory);
+	return 2;
+}
+
+static int lua_iteratedirectory(lua_State* L)
+{
+	size_t len;
+	auto text = luaL_checklstring(L, 1, &len);
+	auto strPath = UTF8ToUTF16(text, len) + L"\\*";
+	WIN32_FIND_DATAW FindFileData;
+	HANDLE hFind = FindFirstFileW(strPath.c_str(), &FindFileData);
+	auto bCompleted = true;
+	if (hFind != INVALID_HANDLE_VALUE)
+	{
+		for (;;)
+		{
+			std::wstring strName = FindFileData.cFileName;
+			if (strName != L".." && strName != L".")
+			{
+				auto bDirectory = (FindFileData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) != 0;
+
+				lua_pushvalue(L, 2);
+				lua_pushlstring(L, strName);
+				lua_pushboolean(L, bDirectory);
+				lua_call(L, 2, 1);
+				auto bResult = lua_toboolean(L, -1) != 0;
+				lua_pop(L, 1);
+				if (!bResult)
+				{
+					bCompleted = false;
+					break;
+				}
+			}
+			if (!FindNextFileW(hFind, &FindFileData))
+				break;
+		}
+		FindClose(hFind);
+	}
+	lua_pushboolean(L, bCompleted);
+	return 1;
+}
+#pragma endregion
+
+#pragma region LuaOpen
+static const struct luaL_Reg Clipboard[] = {
+	{"GetText", lua_getclipboard},
+	{"SetText", lua_setclipboard},
+	{NULL, NULL}
+};
+static const struct luaL_Reg MsgBox[] = {
+	{"Show", lua_msgbox},
+	{NULL, NULL}
+};
+static const struct luaL_Reg IO[] = {
+	{"CreateDirectory", lua_createdirectory},
+	{"Exists", lua_exists},
+	{"IterateDirectory", lua_iteratedirectory},
+	{NULL, NULL}
+};
+
+extern "C" __declspec(dllexport) int luaopen_ProddyUtils(lua_State * L)
+{
+	lua_newtable(L);
+	luaL_newlib(L, Clipboard);
+	lua_setfield(L, -2, "Clipboard");
+	luaL_newlib(L, IO);
+	lua_setfield(L, -2, "IO");
+	luaL_newlib(L, MsgBox);
+	lua_newtable(L);
+	lua_pushinteger(L, 1);
+	lua_setfield(L, -2, "OK");
+	lua_pushinteger(L, 2);
+	lua_setfield(L, -2, "OKCancel");
+	lua_pushinteger(L, 3);
+	lua_setfield(L, -2, "RetryCancel");
+	lua_pushinteger(L, 4);
+	lua_setfield(L, -2, "YesNo");
+	lua_pushinteger(L, 5);
+	lua_setfield(L, -2, "YesNoCancel");
+	lua_setfield(L, -2, "Buttons");
+	lua_newtable(L);
+	lua_pushinteger(L, 1);
+	lua_setfield(L, -2, "OK");
+	lua_pushinteger(L, 2);
+	lua_setfield(L, -2, "Cancel");
+	lua_pushinteger(L, 3);
+	lua_setfield(L, -2, "Retry");
+	lua_pushinteger(L, 4);
+	lua_setfield(L, -2, "Yes");
+	lua_pushinteger(L, 5);
+	lua_setfield(L, -2, "No");
+	lua_setfield(L, -2, "DialogResult");
+	lua_setfield(L, -2, "MessageBox");
+	return 1;
+}
+#pragma endregion
