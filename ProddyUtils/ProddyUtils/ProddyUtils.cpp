ebcc071033260b34a2310948f50d8989b93c4fef
Proddy3 <tehmcterrorist@gmail.com>
2020-04-28 22:22:17 +0100

    v1.4
    - Added Keyboard.IsKeyPressed
    - Added Keyboard.KeyDown
    - Added Keyboard.KeyUp
    - Added Keyboard.Keys
    - Added Keyboard.DXKeys
    - Added Time.GetTimeMillis
    - Added Time.GetTimeMicro
    - Added Time.GetTimeNano
    - Moved initialise notification to debug build only

--- ProddyUtils/ProddyUtils/ProddyUtils.cpp
+++ ProddyUtils/ProddyUtils/ProddyUtils.cpp
@@ -2,11 +2,12 @@
 #include <string>
 #include <experimental/filesystem>
 #include <sstream>
+#include <chrono>
 #include "lua.hpp"
 
 #pragma region Version
 static const int Major = 1;
-static const int Minor = 3;
+static const int Minor = 4;
 static const int Build = 0;
 
 static int lua_checkversion(lua_State* L)
@@ -420,6 +421,77 @@ static int lua_getfiles(lua_State* L) {
 }
 #pragma endregion
 
+#pragma region Keyboard
+static int lua_iskeypressed(lua_State* L)
+{
+	auto size = lua_gettop(L);
+	if (size == 0)
+		return 0;
+	for (int i = 0; i < size; i++) {
+		int key = (int)luaL_checkinteger(L, i + 1);
+		if (!(GetKeyState(key) & 0x8000))
+		{
+			lua_pushboolean(L, false);
+			return 1;
+		}
+	}
+	lua_pushboolean(L, true);
+	return 1;
+}
+static int lua_keydown(lua_State* L)
+{
+	auto size = lua_gettop(L);
+	if (size == 0)
+		return 0;
+	for (int i = 0; i < size; i++) {
+		int key = (int)luaL_checkinteger(L, i + 1);
+		INPUT ip;
+		ip.type = INPUT_KEYBOARD;
+		ip.ki.wScan = key;
+		ip.ki.dwFlags = 0;
+		ip.ki.time = 0;
+		ip.ki.dwExtraInfo = 0;
+		SendInput(1, &ip, sizeof(INPUT));
+	}
+	return 0;
+}
+static int lua_keyup(lua_State* L)
+{
+	auto size = lua_gettop(L);
+	if (size == 0)
+		return 0;
+	for (int i = 0; i < size; i++) {
+		int key = (int)luaL_checkinteger(L, i + 1);
+		INPUT ip;
+		ip.type = INPUT_KEYBOARD;
+		ip.ki.wScan = key;
+		ip.ki.dwFlags = KEYEVENTF_KEYUP;
+		ip.ki.time = 0;
+		ip.ki.dwExtraInfo = 0;
+		SendInput(1, &ip, sizeof(INPUT));
+	}
+	return 0;
+}
+#pragma endregion
+
+#pragma region Time
+static int lua_gettimemillis(lua_State* L)
+{
+	lua_pushinteger(L, std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count());
+	return 1;
+}
+static int lua_gettimemicro(lua_State* L)
+{
+	lua_pushinteger(L, std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count());
+	return 1;
+}
+static int lua_gettimenano(lua_State* L)
+{
+	lua_pushinteger(L, std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count());
+	return 1;
+}
+#pragma endregion
+
 #pragma region LuaOpen
 static const struct luaL_Reg ProddyUtils[] = {
 	{"CheckVersion", lua_checkversion},
@@ -429,6 +501,12 @@ static const struct luaL_Reg ProddyUtils[] = {
 static const struct luaL_Reg Clipboard[] = {
 	{"GetText", lua_getclipboard},
 	{"SetText", lua_setclipboard},
+	{NULL, NULL}
+};
+static const struct luaL_Reg Keyboard[] = {
+	{"IsKeyPressed", lua_iskeypressed},
+	{"KeyDown", lua_keydown},
+	{"KeyUp", lua_keyup},
 	{NULL, NULL}
 };
 static const struct luaL_Reg MsgBox[] = {
@@ -444,14 +522,351 @@ static const struct luaL_Reg IO[] = {
 	{"IterateDirectory", lua_iteratedirectory},
 	{NULL, NULL}
 };
+static const struct luaL_Reg Time[] = {
+	{"GetTimeMillis", lua_gettimemillis},
+	{"GetTimeMicro", lua_gettimemicro},
+	{"GetTimeNano", lua_gettimenano},
+	{NULL, NULL}
+};
 
 extern "C" __declspec(dllexport) int luaopen_ProddyUtils(lua_State * L)
 {
 	luaL_newlib(L, ProddyUtils);
+
 	luaL_newlib(L, Clipboard);
 	lua_setfield(L, -2, "Clipboard");
+
 	luaL_newlib(L, IO);
 	lua_setfield(L, -2, "IO");
+
+	luaL_newlib(L, Time);
+	lua_setfield(L, -2, "Time");
+
+	luaL_newlib(L, Keyboard);
+	lua_newtable(L);
+	std::string keystring;
+	char keybuffer;
+	for (int i = 0; i < 256; ++i) {
+		if (i == 3 || i == 8 || i == 9 || i == 13 || i == 27 || i == 32)
+			continue;
+		if (keybuffer = MapVirtualKey(UINT(i), 2)) {
+			keystring += keybuffer;
+			lua_pushinteger(L, i);
+			lua_setfield(L, -2, keystring.c_str());
+			keystring = "";
+		}
+	}
+	lua_pushinteger(L, 0x1B);
+	lua_setfield(L, -2, "Escape");
+	lua_pushinteger(L, 0x70);
+	lua_setfield(L, -2, "F1");
+	lua_pushinteger(L, 0x71);
+	lua_setfield(L, -2, "F2");
+	lua_pushinteger(L, 0x72);
+	lua_setfield(L, -2, "F3");
+	lua_pushinteger(L, 0x73);
+	lua_setfield(L, -2, "F4");
+	lua_pushinteger(L, 0x74);
+	lua_setfield(L, -2, "F5");
+	lua_pushinteger(L, 0x75);
+	lua_setfield(L, -2, "F6");
+	lua_pushinteger(L, 0x76);
+	lua_setfield(L, -2, "F7");
+	lua_pushinteger(L, 0x77);
+	lua_setfield(L, -2, "F8");
+	lua_pushinteger(L, 0x78);
+	lua_setfield(L, -2, "F9");
+	lua_pushinteger(L, 0x79);
+	lua_setfield(L, -2, "F10");
+	lua_pushinteger(L, 0x7A);
+	lua_setfield(L, -2, "F11");
+	lua_pushinteger(L, 0x7B);
+	lua_setfield(L, -2, "F12");
+	lua_pushinteger(L, 0x2C);
+	lua_setfield(L, -2, "PrintScreen");
+	lua_pushinteger(L, 0x91);
+	lua_setfield(L, -2, "ScrollLock");
+	lua_pushinteger(L, 0x08);
+	lua_setfield(L, -2, "Backspace");
+	lua_pushinteger(L, 0x20);
+	lua_setfield(L, -2, "Space");
+	lua_pushinteger(L, 0x2D);
+	lua_setfield(L, -2, "Insert");
+	lua_pushinteger(L, 0x24);
+	lua_setfield(L, -2, "Home");
+	lua_pushinteger(L, 0x22);
+	lua_setfield(L, -2, "PageDown");
+	lua_pushinteger(L, 0x21);
+	lua_setfield(L, -2, "PageUp");
+	lua_pushinteger(L, 0x2E);
+	lua_setfield(L, -2, "Delete");
+	lua_pushinteger(L, 0x90);
+	lua_setfield(L, -2, "Numlock");
+	lua_pushinteger(L, 0x6F);
+	lua_setfield(L, -2, "Numpad /");
+	lua_pushinteger(L, 0x6A);
+	lua_setfield(L, -2, "Numpad*");
+	lua_pushinteger(L, 0x6D);
+	lua_setfield(L, -2, "Numpad-");
+	lua_pushinteger(L, 0x6B);
+	lua_setfield(L, -2, "Numpad+");
+	lua_pushinteger(L, 0x6E);
+	lua_setfield(L, -2, "Numpad.");
+	lua_pushinteger(L, 0x60);
+	lua_setfield(L, -2, "Numpad0");
+	lua_pushinteger(L, 0x61);
+	lua_setfield(L, -2, "Numpad1");
+	lua_pushinteger(L, 0x62);
+	lua_setfield(L, -2, "Numpad2");
+	lua_pushinteger(L, 0x63);
+	lua_setfield(L, -2, "Numpad3");
+	lua_pushinteger(L, 0x64);
+	lua_setfield(L, -2, "Numpad4");
+	lua_pushinteger(L, 0x65);
+	lua_setfield(L, -2, "Numpad5");
+	lua_pushinteger(L, 0x66);
+	lua_setfield(L, -2, "Numpad6");
+	lua_pushinteger(L, 0x67);
+	lua_setfield(L, -2, "Numpad7");
+	lua_pushinteger(L, 0x68);
+	lua_setfield(L, -2, "Numpad8");
+	lua_pushinteger(L, 0x68);
+	lua_setfield(L, -2, "Numpad9");
+
+	lua_pushinteger(L, 0x26);
+	lua_setfield(L, -2, "ArrowUp");
+	lua_pushinteger(L, 0x28);
+	lua_setfield(L, -2, "ArrowDown");
+	lua_pushinteger(L, 0x25);
+	lua_setfield(L, -2, "ArrowLeft");
+	lua_pushinteger(L, 0x27);
+	lua_setfield(L, -2, "ArrowRight");
+
+	lua_pushinteger(L, 0x0D);
+	lua_setfield(L, -2, "Enter");
+	lua_pushinteger(L, 0x10);
+	lua_setfield(L, -2, "Shift");
+	lua_pushinteger(L, 0xA0);
+	lua_setfield(L, -2, "LShift");
+	lua_pushinteger(L, 0xA1);
+	lua_setfield(L, -2, "RShift");
+	lua_pushinteger(L, 0x09);
+	lua_setfield(L, -2, "Tab");
+	lua_pushinteger(L, 0x14);
+	lua_setfield(L, -2, "CapsLock");
+	lua_pushinteger(L, 0x11);
+	lua_setfield(L, -2, "Control");
+	lua_pushinteger(L, 0xA2);
+	lua_setfield(L, -2, "LControl");
+	lua_pushinteger(L, 0xA3);
+	lua_setfield(L, -2, "RControl");
+	lua_pushinteger(L, 0x12);
+	lua_setfield(L, -2, "Alt");
+	lua_pushinteger(L, 0xA4);
+	lua_setfield(L, -2, "LAlt");
+	lua_pushinteger(L, 0xA5);
+	lua_setfield(L, -2, "RAlt");
+	lua_pushinteger(L, 0x5B);
+	lua_setfield(L, -2, "LWin");
+	lua_pushinteger(L, 0x5C);
+	lua_setfield(L, -2, "RWin");
+	lua_setfield(L, -2, "Keys");
+	lua_newtable(L);
+	lua_pushinteger(L, 1);
+	lua_setfield(L, -2, "Escape");
+	lua_pushinteger(L, 2);
+	lua_setfield(L, -2, "1");
+	lua_pushinteger(L, 3);
+	lua_setfield(L, -2, "2");
+	lua_pushinteger(L, 4);
+	lua_setfield(L, -2, "3");
+	lua_pushinteger(L, 6);
+	lua_setfield(L, -2, "5");
+	lua_pushinteger(L, 8);
+	lua_setfield(L, -2, "7");
+	lua_pushinteger(L, 10);
+	lua_setfield(L, -2, "9");
+	lua_pushinteger(L, 11);
+	lua_setfield(L, -2, "0");
+	lua_pushinteger(L, 12);
+	lua_setfield(L, -2, "Minus");
+	lua_pushinteger(L, 13);
+	lua_setfield(L, -2, "Equals");
+	lua_pushinteger(L, 14);
+	lua_setfield(L, -2, "Backspace");
+	lua_pushinteger(L, 15);
+	lua_setfield(L, -2, "Tab");
+	lua_pushinteger(L, 16);
+	lua_setfield(L, -2, "Q");
+	lua_pushinteger(L, 17);
+	lua_setfield(L, -2, "W");
+	lua_pushinteger(L, 18);
+	lua_setfield(L, -2, "E");
+	lua_pushinteger(L, 19);
+	lua_setfield(L, -2, "R");
+	lua_pushinteger(L, 20);
+	lua_setfield(L, -2, "T");
+	lua_pushinteger(L, 21);
+	lua_setfield(L, -2, "Y");
+	lua_pushinteger(L, 22);
+	lua_setfield(L, -2, "U");
+	lua_pushinteger(L, 23);
+	lua_setfield(L, -2, "I");
+	lua_pushinteger(L, 24);
+	lua_setfield(L, -2, "O");
+	lua_pushinteger(L, 25);
+	lua_setfield(L, -2, "P");
+	lua_pushinteger(L, 26);
+	lua_setfield(L, -2, "LeftBracket");
+	lua_pushinteger(L, 27);
+	lua_setfield(L, -2, "RightBracket");
+	lua_pushinteger(L, 28);
+	lua_setfield(L, -2, "Enter");
+	lua_pushinteger(L, 29);
+	lua_setfield(L, -2, "LeftControl");
+	lua_pushinteger(L, 30);
+	lua_setfield(L, -2, "A");
+	lua_pushinteger(L, 31);
+	lua_setfield(L, -2, "S");
+	lua_pushinteger(L, 32);
+	lua_setfield(L, -2, "D");
+	lua_pushinteger(L, 33);
+	lua_setfield(L, -2, "F");
+	lua_pushinteger(L, 34);
+	lua_setfield(L, -2, "G");
+	lua_pushinteger(L, 35);
+	lua_setfield(L, -2, "H");
+	lua_pushinteger(L, 36);
+	lua_setfield(L, -2, "J");
+	lua_pushinteger(L, 37);
+	lua_setfield(L, -2, "K");
+	lua_pushinteger(L, 38);
+	lua_setfield(L, -2, "L");
+	lua_pushinteger(L, 39);
+	lua_setfield(L, -2, "Semicolon");
+	lua_pushinteger(L, 40);
+	lua_setfield(L, -2, "Apostrophe");
+	lua_pushinteger(L, 41);
+	lua_setfield(L, -2, "Tilde");
+	lua_pushinteger(L, 42);
+	lua_setfield(L, -2, "LeftShift");
+	lua_pushinteger(L, 43);
+	lua_setfield(L, -2, "BackSlash");
+	lua_pushinteger(L, 44);
+	lua_setfield(L, -2, "Z");
+	lua_pushinteger(L, 45);
+	lua_setfield(L, -2, "X");
+	lua_pushinteger(L, 46);
+	lua_setfield(L, -2, "C");
+	lua_pushinteger(L, 47);
+	lua_setfield(L, -2, "V");
+	lua_pushinteger(L, 48);
+	lua_setfield(L, -2, "B");
+	lua_pushinteger(L, 49);
+	lua_setfield(L, -2, "N");
+	lua_pushinteger(L, 50);
+	lua_setfield(L, -2, "M");
+	lua_pushinteger(L, 51);
+	lua_setfield(L, -2, "Comma");
+	lua_pushinteger(L, 52);
+	lua_setfield(L, -2, "Period");
+	lua_pushinteger(L, 53);
+	lua_setfield(L, -2, "ForwardSlash");
+	lua_pushinteger(L, 54);
+	lua_setfield(L, -2, "RightShift");
+	lua_pushinteger(L, 55);
+	lua_setfield(L, -2, "NumpadMultiply");
+	lua_pushinteger(L, 56);
+	lua_setfield(L, -2, "LeftAlt");
+	lua_pushinteger(L, 57);
+	lua_setfield(L, -2, "Spacebar");
+	lua_pushinteger(L, 58);
+	lua_setfield(L, -2, "CapsLock");
+	lua_pushinteger(L, 59);
+	lua_setfield(L, -2, "F1");
+	lua_pushinteger(L, 60);
+	lua_setfield(L, -2, "F2");
+	lua_pushinteger(L, 61);
+	lua_setfield(L, -2, "F3");
+	lua_pushinteger(L, 62);
+	lua_setfield(L, -2, "F4");
+	lua_pushinteger(L, 63);
+	lua_setfield(L, -2, "F5");
+	lua_pushinteger(L, 64);
+	lua_setfield(L, -2, "F6");
+	lua_pushinteger(L, 65);
+	lua_setfield(L, -2, "F7");
+	lua_pushinteger(L, 66);
+	lua_setfield(L, -2, "F8");
+	lua_pushinteger(L, 67);
+	lua_setfield(L, -2, "F9");
+	lua_pushinteger(L, 68);
+	lua_setfield(L, -2, "F10");
+	lua_pushinteger(L, 69);
+	lua_setfield(L, -2, "NumLock");
+	lua_pushinteger(L, 70);
+	lua_setfield(L, -2, "ScrollLock");
+	lua_pushinteger(L, 71);
+	lua_setfield(L, -2, "Numpad7");
+	lua_pushinteger(L, 72);
+	lua_setfield(L, -2, "Numpad8");
+	lua_pushinteger(L, 73);
+	lua_setfield(L, -2, "Numpad9");
+	lua_pushinteger(L, 74);
+	lua_setfield(L, -2, "NumpadMinus");
+	lua_pushinteger(L, 75);
+	lua_setfield(L, -2, "Numpad4");
+	lua_pushinteger(L, 76);
+	lua_setfield(L, -2, "Numpad5");
+	lua_pushinteger(L, 77);
+	lua_setfield(L, -2, "Numpad6");
+	lua_pushinteger(L, 78);
+	lua_setfield(L, -2, "NumpadPlus");
+	lua_pushinteger(L, 79);
+	lua_setfield(L, -2, "Numpad1");
+	lua_pushinteger(L, 80);
+	lua_setfield(L, -2, "Numpad2");
+	lua_pushinteger(L, 81);
+	lua_setfield(L, -2, "Numpad3");
+	lua_pushinteger(L, 82);
+	lua_setfield(L, -2, "Numpad0");
+	lua_pushinteger(L, 83);
+	lua_setfield(L, -2, "NumpadPeriod");
+	lua_pushinteger(L, 87);
+	lua_setfield(L, -2, "F11");
+	lua_pushinteger(L, 88);
+	lua_setfield(L, -2, "F12");
+	lua_pushinteger(L, 156);
+	lua_setfield(L, -2, "NumpadEnter");
+	lua_pushinteger(L, 157);
+	lua_setfield(L, -2, "RightControl");
+	lua_pushinteger(L, 181);
+	lua_setfield(L, -2, "NumpadSlash");
+	lua_pushinteger(L, 184);
+	lua_setfield(L, -2, "RightAlt");
+	lua_pushinteger(L, 199);
+	lua_setfield(L, -2, "Home");
+	lua_pushinteger(L, 200);
+	lua_setfield(L, -2, "UpArrow");
+	lua_pushinteger(L, 201);
+	lua_setfield(L, -2, "PageUp");
+	lua_pushinteger(L, 203);
+	lua_setfield(L, -2, "LeftArrow");
+	lua_pushinteger(L, 205);
+	lua_setfield(L, -2, "RightArrow");
+	lua_pushinteger(L, 207);
+	lua_setfield(L, -2, "End");
+	lua_pushinteger(L, 208);
+	lua_setfield(L, -2, "DownArrow");
+	lua_pushinteger(L, 209);
+	lua_setfield(L, -2, "PageDown");
+	lua_pushinteger(L, 210);
+	lua_setfield(L, -2, "Insert");
+	lua_pushinteger(L, 211);
+	lua_setfield(L, -2, "Delete");
+	lua_setfield(L, -2, "DXKeys");
+	lua_setfield(L, -2, "Keyboard");
+
 	luaL_newlib(L, MsgBox);
 	lua_newtable(L);
 	lua_pushinteger(L, 1);
@@ -479,6 +894,7 @@ extern "C" __declspec(dllexport) int luaopen_ProddyUtils(lua_State * L)
 	lua_setfield(L, -2, "DialogResult");
 	lua_setfield(L, -2, "MessageBox");
 
+#if DEBUG
 	lua_getglobal(L, "ui");
 	lua_getfield(L, -1, "notify_above_map");
 	std::stringstream ss;
@@ -490,6 +906,8 @@ extern "C" __declspec(dllexport) int luaopen_ProddyUtils(lua_State * L)
 		lua_pop(L, 1);
 	}
 	lua_pop(L, 1);
+#endif
+
 	return 1;
 }
 #pragma endregion
